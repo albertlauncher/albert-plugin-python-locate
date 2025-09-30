d27b5d31eb43e5ace6033ca267355cc21a682d54
Manuel Schneider <manuelschneid3r@gmail.com>
2025-10-01 01:13:19 +0200

    chore: Adopt v4 API changes

--- __init__.py
+++ __init__.py
@@ -5,9 +5,10 @@ import shlex
 import subprocess
 from pathlib import Path
 
-from albert import *
+from albert import (PluginInstance, TriggerQueryHandler, StandardItem, Action, openFile,
+                    makeFileTypeIcon, makeGraphemeIcon, makeComposedIcon, Matcher)
 
-md_iid = "3.0"
+md_iid = "4.0"
 md_version = "3.1"
 md_name = "Locate"
 md_description = "Find files using locate"
@@ -15,7 +16,7 @@ md_license = "MIT"
 md_url = "https://github.com/albertlauncher/albert-plugin-python-locate"
 md_bin_dependencies = ["locate"]
 md_authors = ["@ManuelSchneid3r"]
-md_maintainers = ["@ManuelSchneid3r"]
+
 
 
 class Plugin(PluginInstance, TriggerQueryHandler):
@@ -23,14 +24,6 @@ class Plugin(PluginInstance, TriggerQueryHandler):
     def __init__(self):
         PluginInstance.__init__(self)
         TriggerQueryHandler.__init__(self)
-
-        self.iconUrls = [
-            "xdg:preferences-system-search",
-            "xdg:system-search",
-            "xdg:search",
-            "xdg:text-x-generic",
-            f"file:{Path(__file__).parent}/locate.svg"
-        ]
 
     def defaultTrigger(self):
         return "'"
@@ -46,7 +39,7 @@ class Plugin(PluginInstance, TriggerQueryHandler):
             # Fetch results from locate and filter them using Matcher
 
             matcher = Matcher(query.string)
-            rank_items = []
+            items = []
             with subprocess.Popen(['locate', *args], stdout=subprocess.PIPE, text=True) as proc:
                 for line in proc.stdout:
                     if not query.isValid:
@@ -55,29 +48,27 @@ class Plugin(PluginInstance, TriggerQueryHandler):
                     path = line.strip()
                     filename = Path(path).name
                     if m := matcher.match(filename, path):
-                        rank_items.append(
-                            RankItem(
-                                StandardItem(
-                                    id=path,
-                                    text=filename,
-                                    subtext=path,
-                                    iconUrls=[f"qfip:{path}"],
-                                    actions=[
-                                        Action("open", "Open", lambda p=path: openUrl("file://%s" % p))
-                                    ]
-                                ),
-                                m
-                            )
-                        )
+                        items.append((
+                            StandardItem(
+                                id=path,
+                                text=filename,
+                                subtext=path,
+                                iconFactory=lambda: makeFileTypeIcon(path),
+                                actions=[
+                                    Action("open", "Open", lambda p=path: openFile(p))
+                                ]
+                            ),
+                            float(m)
+                        ))
 
             # Filter using the matcher
 
-            rank_items = sorted(rank_items, key=lambda x: x.score, reverse=True)
+            items = sorted(items, key=lambda x: x[1], reverse=True)
 
             if not query.isValid:
                 return
 
-            query.add([ri.item for ri in rank_items])
+            query.add([i[0] for i in items])
 
         else:
             query.add(
@@ -85,6 +76,6 @@ class Plugin(PluginInstance, TriggerQueryHandler):
                     id="locate.info",
                     text="Token is too short",
                     subtext="Each token must have at least three characters",
-                    iconUrls=self.iconUrls
+                    iconFactory=lambda: makeComposedIcon(makeGraphemeIcon("🔎"), makeGraphemeIcon("⚠️"), 1.0)
                 )
             )
